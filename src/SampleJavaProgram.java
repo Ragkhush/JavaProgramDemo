336ccc234c379581ee33714e4e2b7792b47539b2
RAGINI MISHRA <RAGINI MISHRA@LAPTOP-6CT0OBJ8>
2019-07-27 21:12:34 +0530

    Java String program

---
+++ src/SampleJavaProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author RAGINI MISHRA
+ *
+ */
+public class SampleJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String str="Ragini Mishra";
+		System.out.println(str);
+
+	}
+
+}
